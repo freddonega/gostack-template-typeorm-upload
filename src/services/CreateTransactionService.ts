522c9e60ac93793a78369af8c6638434a83a9754
Fred Donega <fred.donega@mtistudio.com>
2020-05-11 17:42:53 -0300

    entrega do desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,66 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  category: string;
+  type: 'income' | 'outcome';
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    category,
+    type,
+  }: Request): Promise<Transaction> {
+    const categoriesRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    if (type === 'outcome') {
+      const { total } = await transactionsRepository.getBalance();
+      if (value > total)
+        throw new AppError(
+          'Cannot create an outcome transaction without enough balance',
+          400,
+        );
+    }
+
+    const checkCategoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    let category_id = null;
+
+    if (checkCategoryExists) {
+      category_id = checkCategoryExists.id;
+    } else {
+      const newCategory = await categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(newCategory);
+
+      category_id = newCategory?.id;
+    }
+
+    const transaction = await transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
